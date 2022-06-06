6774384c079db14454ae758662970c482326deab
RandoM <danik.tim@gmail.com>
2022-06-06 22:39:28 +0300

    massive provider refactor and code cleaning, now the sockets actually work everywhere

--- test/new_test.dart
+++ test/new_test.dart
@@ -1,13 +1,24 @@
+import 'dart:async';
+
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/status.dart' as status;
 
-void main() async {
-  var channel = IOWebSocketChannel.connect(Uri.parse('wss://streamer.finance.yahoo.com/'));
-  channel.sink.add('{"subscribe":["AMZN"]}');
-  // var channel = IOWebSocketChannel.connect(Uri.parse('ws://localhost:1234'));
+final channel =
+    IOWebSocketChannel.connect(Uri.parse('wss://streamer.finance.yahoo.com/'));
+final channelController = StreamController.broadcast();
+late StreamSubscription channelSubscriber;
 
-  channel.stream.listen((message) {
-    channel.sink.add('received!');
-    channel.sink.close(status.goingAway);
+void main() async {
+  final ticker = 'AAPL';
+  channelController.addStream(channel.stream);
+  channel.sink.add('{"subscribe":["$ticker"]}');
+  // var channel = IOWebSocketChannel.connect(Uri.parse('ws://localhost:1234'));
+  // channel.stream.listen((message) {
+  //   channel.sink.add('received!');
+  //   channel.sink.close(status.goingAway);
+  // });
+
+  channelSubscriber = channelController.stream.listen((data) {
+    print(data);
   });
 }
